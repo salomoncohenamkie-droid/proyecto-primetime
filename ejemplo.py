569b58bff68e1cb18d40e746d5e904f15eb8af03
salomoncohenamkie-droid <salomoncohenamkie@gmail.com>
2025-11-06 09:56:06 -0600

    borrar codigo, pegar codigo
    Added functionality for managing institutions, students, and subjects with grading criteria.

--- ejemplo.py
+++ ejemplo.py
@@ -1,65 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 28 10:19:57 2025
+Created on Thu Nov  6 09:26:55 2025
 
 @author: 260987
 """
-
-import pygame  
-import random  
-  
-def button(surface):  
-    x, y = pygame.mouse.get_pos()  
-    rect_coordinates = (50, 50, 50, 50)  
-    def inside_rectangle(x, y, rect):  
-        xr, yr, width, height = rect  
-        return (  
-            xr <= x and x <= (xr + width) and   
-            yr <= y and y <= (yr + height)  
-        )  
-    if inside_rectangle(x, y, rect_coordinates):  
-        pygame.draw.rect(  
-            surface,  
-            color="gray67",  
-            rect=rect_coordinates,  
-            width=0,  
-            border_radius=10  
-        )  
-    else:  
-        pygame.draw.rect(  
-            surface,  
-            color="blue",  
-            rect=rect_coordinates,  
-            width=0,  
-            border_radius=10  
-        )  
-    return rect_coordinates  
-  
-if __name__ == "__main__":  
-    pygame.init()  
-    screen = pygame.display.set_mode((1280, 720))  
-    clock = pygame.time.Clock()  
-    running = True  
-    color = "goldenrod4"  
-      
-    while running:  
-        for event in pygame.event.get():  
-            if event.type == pygame.QUIT:  
-                running = False  
-            elif event.type == pygame.MOUSEBUTTONDOWN:  
-                # Check if click is inside button  
-                x, y = event.pos  
-                rect_coordinates = (50, 50, 50, 50)  
-                xr, yr, width, height = rect_coordinates  
-                if xr <= x <= (xr + width) and yr <= y <= (yr + height):  
-                    # Generate random color  
-                    color = (random.randint(0, 255),   
-                            random.randint(0, 255),   
-                            random.randint(0, 255)) 
-                  
-        screen.fill(color)  
-        button(screen)  
-        pygame.display.flip()  
-        clock.tick(60)  
-          
-    pygame.quit()
+"""
+#   Permite agregar instituciones.
+#   Permite agregar estudiantes dentro de las instituciones.
+#   Permite agregar materias para cada institución
+#   Permite guardar los criterios para calificar la materia.
+#   Advierte si la ponderación es diferente de 100%
+    -   Si la calificación ponderada es mayor a 10, la calificación es 10.
+-   Guarda las materias que ya se le dieron al programa.
+-   Calcula la calificación por materia.
+-   Calcula la calificación global.
+-   Calcula la calificación máxima con las entregas hasta el momento.
+-   Te dice qué necesitas sacar en una materia para llegar a tu calificación objetivo.
+-   Tiene una interfaz para cada actividad.
+-   Gestiona correctamente entrada de datos incorrectos.
+"""
